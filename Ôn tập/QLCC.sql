ae50708148af5173bff622d6b38d7d92c453a4ae
UIT19522006 <67238266+UIT19522006@users.noreply.github.com>
2020-12-21 01:43:13 +0700

    Bài tập ôn tập

---
+++ Ôn tập/QLCC.sql
@@ -0,0 +1,144 @@
+﻿create database QLCC
+go
+use QLCC
+create table KHACHHANG
+(
+	MAKH char(4) not null,
+	TENKH varchar(255),
+	DIACHI varchar(20),
+	LOAIKH varchar(20)
+)
+create table LOAICAY
+(
+	MALC char(4) not null,
+	TENLC varchar(50),
+	XUATXU varchar(20),
+	GIA money
+)
+set dateformat DMY
+create table HOADON
+(
+	SOHD int not null,
+	NGHD smalldatetime,
+	MAKH char(4) not null,
+	KHUYENMAI int
+)
+create table CTHD 
+(
+	SOHD int not null,
+	MALC char(4) not null,
+	SOLUONG int
+)
+-- Chèn khóa chính
+alter table KHACHHANG add constraint PK_MAKH primary key (MAKH)
+alter table LOAICAY add constraint PK_MALC primary key (MALC)
+alter table HOADON add constraint PK_SOHD primary key (SOHD)
+alter table CTHD add constraint PK_SOHD_MALC primary key (SOHD,MALC)
+--Chèn khóa ngoại
+alter table HOADON
+add constraint FK_MAKH foreign key(MAKH) references KHACHHANG(MAKH)
+alter table CTHD
+add constraint FK_SOHD foreign key(SOHD) references HOADON(SOHD)
+alter table CTHD
+add constraint FK_MALC foreign key(MALC) references LOAICAY(MALC)
+
+--use master drop database QLCC
+
+-- Câu 2: Nhập dữ liêu:
+insert into KHACHHANG
+(MAKH, TENKH, DIACHI, LOAIKH)
+values
+('KH01','Liz Kim Cuong', 'Ha Noi','Vang lai'),
+('KH02','Ivone Dieu Linh', 'Da Nang','Thuong Xuyen'),
+('KH03','Liz Kim Cuong', 'TP.HCM','Vang lai')
+
+insert into LOAICAY
+(MALC, TENLC, XUATXU, GIA)
+values
+('LC01', 'Xuong rong tai tho', 'Mexico', 180000),
+('LC02', 'Sen thach ngoc ','Anh', 300000),
+('LC03' ,'Ba mau rau' ,'Nam Phi', 270000)
+set dateformat DMY
+insert into HOADON
+	(SOHD,NGHD,MAKH,KHUYENMAI)
+values
+('00001','22/11/2017', 'KH01', 5),
+('00002', '04/12/2017', 'KH03', 5),
+('00003', '10/12/2017', 'KH02', 10)
+alter table CTHD
+nocheck constraint FK_SOHD
+insert into CTHD
+	(SOHD, MALC, SOLUONG)
+values
+('00001', 'LC01', 1),
+('00001', 'LC02', 2),
+('00003', 'LC03', 5)
+
+-- Câu 3: Hiện thực ràng buộc toàn vẹn sau: Tất cả các mặt hàng xuất xứ từ nước Anh đều có giá lớn hơn 250.000đ (1đ).
+create trigger trigg_HANG_TU_ANH on LOAICAY
+for insert, update
+as
+begin
+	if exists (select * from inserted I where XUATXU='Anh' and GIA <= 250000)
+	begin
+		print N'LỖI: Tất cả các mặt hàng xuất xứ từ nước Anh đều có giá lớn hơn 250.000đ'
+		rollback transaction
+	end
+end
+insert into LOAICAY values ('LC04', 'Cây kem', 'Anh', 1000)
+-- Câu 4: Hiện thực ràng buộc toàn vẹn sau: Hóa đơn mua với số lượng tổng cộng lớn hơn hoặc bằng 5 đều được giảm giá 10 phần trăm(1đ)
+create trigger trigg_GIAMGIATONGSOLUON on CTHD
+for insert, update
+as
+begin
+	declare @SOHD int, @SOLUONG int, @tongsoluong int
+	set @tongsoluong=0
+	select @SOHD=SOHD from inserted
+	declare cur_CTHD cursor
+	for
+		select SOLUONG from CTHD where SOHD=@SOHD 
+	open cur_CTHD
+
+	fetch next from cur_CTHD into @SOLUONG
+	while(@@FETCH_STATUS =0)
+	begin
+		set @tongsoluong=@tongsoluong+@SOLUONG
+		fetch next from cur_CTHD into @SOLUONG
+	end
+	close cur_CTHD
+	deallocate cur_CTHD
+	if(@tongsoluong>5)
+	update HOADON set KHUYENMAI=10  where SOHD=@SOHD
+end
+
+select * from HOADON 
+insert into CTHD 
+values('0001','LC03',100)
+delete from CTHD where SOHD='0001' and MALC='LC03'
+
+-- Câu 5: Tìm tất cả các hóa đơn có ngày lập hóa đơn trong quý 4 năm 2017, sắp xếp kết quả tăng dần theo phần trăm giảm giá (1đ)
+
+select * from HOADON
+where MONTH(NGHD)>10 and YEAR(NGHD)=2017 order by KHUYENMAI 
+
+-- Câu 6. Tìm loại cây có số lượng mua ít nhất trong tháng 12 (1đ).
+select MALC, TENLC
+from LOAICAY where MALC in
+(select MALC from CTHD cthd join HOADON hd on (cthd.SOHD=hd.SOHD) group by MALC having sum(SOLUONG) <=
+(select top 1 sum(SOLUONG) from CTHD cthd join HOADON hd on (cthd.SOHD=hd.SOHD) group by MALC order by sum(SOLUONG) asc))
+
+-- Câu 7. Tìm loại cây mà cả khách thường xuyên (LOAIKH là ‘Thuong xuyen’) và khách vãng lai (LOAIKH là ‘Vang lai’) đều mua. (1đ)
+select cthd.MALC from 
+CTHD cthd join HOADON hd on cthd.SOHD=hd.SOHD 
+join KHACHHANG kh on hd.MAKH=kh.MAKH 
+where LOAIKH='Vang Lai' and cthd.MALC in(select cthd.MALC from 
+											CTHD cthd join HOADON hd on cthd.SOHD=hd.SOHD 
+											join KHACHHANG kh on hd.MAKH=kh.MAKH 
+											where LOAIKH='Thuong Xuyen')
+
+-- Câu 8. Tìm khách hàng đã từng mua tất cả các loại cây (1đ).
+select kh.MAKH, kh.TENKH from KHACHHANG kh join HOADON hd on kh.MAKH=hd.MAKH
+where not exists 
+(select * from LOAICAY lc where not exists (
+	select * from CTHD cthd where cthd.MALC= lc.MALC and hd.SOHD=cthd.SOHD))
+
---
+++ Ôn tập/QLNX.sql
@@ -0,0 +1,109 @@
+﻿create database QLNX
+go
+use QLNX
+create table NHACUNGCAP
+(
+	MANCC char(5) not null,
+	TENNCC varchar(50),
+	QUOCGIA varchar(20),
+	LOAINCC varchar(20)
+)
+create table DUOCPHAM
+(
+	MADP char(4) not null,
+	TENDP varchar(50),
+	LOAIDP varchar(20),
+	GIA money
+)
+set dateformat DMY
+create table PHIEUNHAP
+(
+	SOPN int not null, 
+	NGNHAP smalldatetime,
+	MANCC char(5) not null,
+	LOAINHAP varchar(20)
+)
+create table CTPN
+(
+	SOPN int not null, 
+	MADP char(4) not null,
+	SOLUONG int
+)
+--Tạo khóa chính
+alter table NHACUNGCAP add constraint PK_MANCC primary key (MANCC)
+alter table DUOCPHAM add constraint PK_MADP primary key (MADP)
+alter table PHIEUNHAP add constraint PK_SOPN primary key(SOPN)
+alter table CTPN add constraint PK_SOPN_MADP primary key(SOPN,MADP)
+-- Tạo khóa ngoại 
+alter table PHIEUNHAP 
+add constraint FK_MANCC foreign key(MANCC) references NHACUNGCAP(MANCC)
+alter table CTPN
+add constraint FK_SOPN foreign key(SOPN) references PHIEUNHAP(SOPN)
+alter table CTPN
+add constraint FK_MADP foreign key(MADP) references DUOCPHAM(MADP)
+
+-- Câu 2: Nhập dữ liệu cho 4 table như đề bài (1đ).
+insert into  NHACUNGCAP
+	(MANCC, TENNCC, QUOCGIA, LOAINCC)
+values
+('NCC01', 'Phuc Hung' ,'Viet Nam', 'Thuong xuyen'),
+('NCC02', 'J. B. Pharmaceuticals' ,'India' ,'Vang lai'),
+('NCC03' ,'Sapharco', 'Singapore', 'Vang lai')
+
+insert into DUOCPHAM 
+	(MADP, TENDP, LOAIDP ,GIA)
+values
+('DP01' , 'Thuoc ho PH' ,'Siro', '120000'),
+('DP02' ,'Zecuf Herbal CouchRemedy' ,'Vien nen' ,'200000'),
+('DP03' ,'Cotrim', 'Vien sui', '80000')
+
+
+insert into PHIEUNHAP
+(SOPN, NGNHAP, MANCC, LOAINHAP)
+values
+('00001' ,'22/11/2017' ,'NCC01', 'Noi dia'),
+('00002' ,'04/12/2017' ,'NCC03' ,'Nhap khau'),
+('00003' ,'10/12/2017', 'NCC02' ,'Nhap khau')
+insert into CTPN
+(SOPN, MADP, SOLUONG)
+values
+('00001', 'DP01', 100),
+('00001', 'DP02', 200),
+('00003', 'DP03' ,543)
+
+--Câu 3: Hiện thực ràng buộc toàn vẹn sau: Tất cả các dược phẩm có loại là Siro đều có giá lớn hơn 100000đ (1đ).create trigger trigg_LOAISIRO on DUOCPHAMfor insert, updateasbegin	if exists (select * from inserted I where I.LOAIDP='Siro' and I.GIA <= 100000)	begin		print N'LỖI: Tất cả các dược phẩm có loại là Siro đều có giá lớn hơn 100000đ'		rollback transaction	endendinsert into DUOCPHAM values ('DP04' , 'Mr ' ,'Siro', '12000')--4. Hiện thực ràng buộc toàn vẹn sau: Phiếu nhập của những nhà cung cấp ở những quốc gia khác Việt Nam đều có loại nhập là Nhập khẩu. (2đ).
+create trigger trigg_LOAINHAP on PHIEUNHAP
+for insert, update
+as
+begin
+	if exists (select * from inserted I join NHACUNGCAP ncc on I.MANCC=ncc.MANCC where QUOCGIA <> 'Viet Nam' and I.LOAINHAP<>'Nhap khau')
+	begin		print N'LỖI: Phiếu nhập của những nhà cung cấp ở những quốc gia khác Việt Nam đều có loại nhập là Nhập khẩu'		rollback transaction	end
+end
+insert into PHIEUNHAP
+(SOPN, NGNHAP, MANCC, LOAINHAP)
+values
+('00099' ,'22/11/2017' ,'NCC02', 'Noi dia')
+--5. Tìm tất cả các phiếu nhập có ngày nhập trong tháng 12 năm 2017, sắp xếp kết quả tăng dần theo ngày nhập (1đ).
+select * from PHIEUNHAP p where year(p.NGNHAP)=2017 and month(p.NGNHAP)=12 order by p.NGNHAP asc
+
+--6. Tìm dược phẩm được nhập số lượng nhiều nhất trong năm 2017 (1đ).
+select * from DUOCPHAM where MADP in
+(select dp.MADP
+from DUOCPHAM dp join CTPN ctpn on dp.MADP=ctpn.MADP join 
+PHIEUNHAP pn on ctpn.SOPN=pn.SOPN group by dp.MADP having sum(SOLUONG) >=
+				(select top 1 sum(SOLUONG) 
+				from DUOCPHAM dp join CTPN ctpn on dp.MADP=ctpn.MADP join 
+				PHIEUNHAP pn on ctpn.SOPN=pn.SOPN group by dp.MADP order by sum(SOLUONG) desc))
+  
+--7. Tìm dược phẩm chỉ có nhà cung cấp thường xuyên (LOAINCC là Thuong xuyen) cung cấp, nhà cung cấp vãng lai (LOAINCC là Vang lai) không cung cấp. (1đ).
+select dp.MADP, dp.LOAIDP, dp.TENDP, dp.GIA from DUOCPHAM dp 
+join CTPN ctpn on dp.MADP = ctpn.MADP 
+join PHIEUNHAP pn on pn.SOPN=ctpn.SOPN
+join NHACUNGCAP ncc on pn.MANCC=ncc.MANCC where LOAINCC='Thuong Xuyen'  
+and dp.MADP not in
+		(select dp.MADP from DUOCPHAM dp 
+		join CTPN ctpn on dp.MADP = ctpn.MADP 
+		join PHIEUNHAP pn on pn.SOPN=ctpn.SOPN
+		join NHACUNGCAP ncc on pn.MANCC=ncc.MANCC where LOAINCC='Vang Lai')
+
+--8. Tìm nhà cung cấp đã từng cung cấp tất cả những dược phẩm có giá trên 100000đ trong năm 2017 (1đ).select MANCC from PHIEUNHAP pn where year(pn.NGNHAP)=2017 and not exists	(select * from DUOCPHAM dp where GIA > 100000 and not exists	(select * from CTPN ctpn where pn.SOPN=ctpn.SOPN and dp.MADP=ctpn.MADP))
